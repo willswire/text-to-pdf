6946e4ca0b4da0acd843bd88e8193e8b0ef7be72
William Walker <w_walker@icloud.com>
2023-04-27 04:53:36 +0000

    GPT-4 code improvements

--- src/index.ts
+++ src/index.ts
@@ -6,52 +6,60 @@ export interface Env {
 }
 
 async function retrievePDF(request: Request, env: Env): Promise<Response> {
-	const url = new URL(request.url);
-	const filename = url.pathname.slice(1)
-	const object = await env.PDF_BUCKET.get(filename);
+	try {
+		const url = new URL(request.url);
+		const filename = url.pathname.slice(1)
+		const pdfObject = await env.PDF_BUCKET.get(filename);
 
-	if (object === null) {
-	  return new Response('Object Not Found', { status: 404 });
+		if (!pdfObject) {
+			return new Response('Object Not Found', { status: 404 });
+		}
+
+		const headers = new Headers();
+		pdfObject.writeHttpMetadata(headers);
+		headers.set('etag', pdfObject.httpEtag);
+
+		return new Response(pdfObject.body, { headers });
+	} catch (error) {
+		console.error(error);
+		return new Response('Server Error', { status: 500 });
 	}
-
-	const headers = new Headers();
-	object.writeHttpMetadata(headers);
-	headers.set('etag', object.httpEtag);
-
-	return new Response(object.body, {
-	  headers,
-	});
 }
 
 async function createPDF(request: Request, env: Env): Promise<string> {
-	const document = new jsPDF();
-	const content = await request.text();
-	document.text(content, 10, 10);
-	const value = document.output("arraybuffer");
-	const key = uuidv4() + '.pdf';
-	await env.PDF_BUCKET.put(key, value);
-	return key;
+	try {
+		const pdfDocument = new jsPDF();
+		const content = await request.text();
+		pdfDocument.text(content, 10, 10);
+		const pdfArrayBuffer = pdfDocument.output("arraybuffer");
+		const pdfKey = `${uuidv4()}.pdf`;
+		await env.PDF_BUCKET.put(pdfKey, pdfArrayBuffer);
+		return pdfKey;
+	} catch (error) {
+		console.error(error);
+		return '';
+	}
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 
 		switch (request.method) {
 			case 'POST':
-				const filename = await createPDF(request, env);
-				return new Response(`Success! Your PDF is available at ${url.hostname}/${filename}\n`);
+				try {
+					const filename = await createPDF(request, env);
+					return new Response(`Success! Your PDF is available at ${url.hostname}/${filename}\n`);
+				} catch (error) {
+					return new Response('Failed to create PDF', { status: 500 });
+				}
 			case 'GET':
 				return await retrievePDF(request, env)
 			default:
 				return new Response('Method Not Allowed', {
 					status: 405,
 					headers: {
-						allow: 'PUT, GET, DELETE'
+						allow: 'POST, GET'
 					}
 				})
 		}
